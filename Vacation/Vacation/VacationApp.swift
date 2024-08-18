5e7828b0bd27974fb0dd0badb3c52d2bdf282c56
rupeshmishra90 <rupeshtikapur@gmail.com>
2024-08-18 23:14:34 +0530

    My initial coomit

---
+++ Vacation/Vacation/VacationApp.swift
@@ -0,0 +1,17 @@
+//
+//  VacationApp.swift
+//  Vacation
+//
+//  Created by Rupesh Kumar on 18/08/24.
+//
+
+import SwiftUI
+
+@main
+struct VacationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
